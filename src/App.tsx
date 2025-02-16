20f2f4f8f7fbf68654215074f195319c2e43fbc4
Ilyas Khisyametdinov <hopi@mail.ru>
2025-02-16 10:13:57 +0400

    80 lesson

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <Button>Кнопка</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import styles from "./Button.module.css";
+import { ButtonProps } from "./Button.props";
+import cn from "classnames";
+
+function Button({ children, className, ...props }: ButtonProps) {
+  return (
+    <>
+      <button
+        className={cn(styles.button, styles.accent, className)}
+        {...props}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
+
+export default Button;
