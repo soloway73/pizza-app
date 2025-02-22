10001af53a5c6e056a6c74f42a2176c8b72f323c
Ilyas Khisyametdinov <hopi@mail.ru>
2025-02-22 11:50:35 +0400

    105 lesson

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,17 +3,49 @@ import Button from "../../components/Button/Button";
 import Headling from "../../components/Headling/Headling";
 import Input from "../../components/Input/Input";
 import styles from "./Login.module.css";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { PREFIX } from "../../helpers/API";
+
+export type LoginForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
 
 export function Login() {
-  const submit = (e: FormEvent) => {
+  const [error, setError] = useState<string | null>();
+  const submit = async (e: FormEvent) => {
     e.preventDefault();
-    console.log("submit");
+    setError(null);
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    await sendLogin(email.value, password.value);
   };
+
+  const sendLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post(`${PREFIX}/auth/login`, {
+        email,
+        password,
+      });
+      console.log("data :>> ", data);
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        console.log("e :>> ", e);
+        setError(e.response?.data.message);
+      }
+    }
+  };
+
   return (
     <>
       <form className={styles.form} onSubmit={submit}>
         <Headling>Вход</Headling>
+        {error && <div className={styles.error}>{error}</div>}
         <div className={styles.inputWrapper}>
           <label className={styles.label} htmlFor="email">
             Ваш email
