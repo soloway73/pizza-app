0e01f55bd8186530e043947f6136136b9076cb84
Ilyas Khisyametdinov <hopi@mail.ru>
2025-02-24 14:03:16 +0400

    112 lesson

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { AuthLayout } from "./components/layout/Auth/AuthLayout";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/Register/Register";
 import { RequireAuth } from "./helpers/RequireAuth";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const Menu = lazy(() => import("./pages/Menu/Menu"));
 
@@ -77,6 +79,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
