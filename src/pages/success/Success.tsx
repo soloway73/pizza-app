88784f834fd9a0752ab3c1acdcbd5596f0d78485
Ilyas Khisyametdinov <hopi@mail.ru>
2025-03-01 02:03:43 +0400

    done

--- src/pages/success/Success.tsx
+++ src/pages/success/Success.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
-import styles from "./success.module.css";
+import styles from "./Success.module.css";
 
 export function Success() {
   const navigate = useNavigate();
