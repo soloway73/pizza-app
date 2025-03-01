5b90f159af79aba60e6513a0bd9e59ed96fdda48
Ilyas Khisyametdinov <hopi@mail.ru>
2025-03-01 11:53:16 +0400

    fixed cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Headling from "../../components/Headling/Headling";
 import { AppDispatch, RootState } from "../../store/store";
 import CartItem from "../../components/CartItem/CartItem";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { IProduct } from "../../interfaces/product.interface";
 import axios from "axios";
 import { PREFIX } from "../../helpers/API";
@@ -19,16 +19,19 @@ export function Cart() {
   const jwt = useSelector((s: RootState) => s.user.jwt);
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
-  const total = items
-    .map((i) => {
-      const product = cartProducts.find((p) => p.id === i.id);
-      if (!product) {
-        return 0;
-      }
-      return i.count * product.price;
-    })
-    .reduce((acc, i) => (acc += i));
-
+  const total = useMemo(
+    () =>
+      items
+        .map((i) => {
+          const product = cartProducts.find((p) => p.id === i.id);
+          if (!product) {
+            return 0;
+          }
+          return i.count * product.price;
+        })
+        .reduce((acc, i) => (acc += i), 0),
+    [items, cartProducts]
+  );
   const getItem = async (id: number) => {
     const { data } = await axios.get<IProduct>(`${PREFIX}/products/${id}`);
     return data;
@@ -54,52 +57,73 @@ export function Cart() {
     dispatch(cartActions.clean());
     navigate("/success");
   };
-
+  const navigateToMenu = () => {
+    navigate("/");
+  };
   useEffect(() => {
     loadAllItems();
   }, [items]);
   return (
     <>
       <Headling className={styles.head}>Корзина</Headling>
-      <div className={styles.wrapper}>
-        {items.map((i) => {
-          const product = cartProducts.find((p) => p.id === i.id);
-          if (!product) {
-            return;
-          }
-          return <CartItem key={i.id} count={i.count} {...product} />;
-        })}
-        <div className={styles.line}>
-          <div className={styles.text}>Итог</div>
-          <div className={styles.price}>
-            {total}&nbsp;
-            <span>₽</span>
-          </div>
-        </div>
-        <hr className={styles.hr} />
-        <div className={styles.line}>
-          <div className={styles.text}>Доставка</div>
-          <div className={styles.price}>
-            {DELIVERY_FEE}&nbsp;
-            <span>₽</span>
-          </div>
-        </div>
-        <hr className={styles.hr} />
-        <div className={styles.line}>
-          <div className={styles.text}>
-            Итог <span className={styles.totalCount}>({items.length})</span>
-          </div>
-          <div className={styles.price}>
-            {total + DELIVERY_FEE}&nbsp;
-            <span>₽</span>
-          </div>
-        </div>
-        <div className={styles.checkout}>
-          <Button appearance="big" onClick={checkout}>
-            Оформить
+      {!total && (
+        <div className={styles.emptyCartWrapper}>
+          <img
+            className={styles.emptyCartImg}
+            src="/empty-cart.png"
+            alt="изображение пустой корзины"
+          />
+          <div>Ваша корзина пуста</div>
+          <Button
+            className={styles.toMenuBtn}
+            appearance="big"
+            onClick={navigateToMenu}
+          >
+            За покупками!
           </Button>
         </div>
-      </div>
+      )}
+      {total > 0 && (
+        <div className={styles.wrapper}>
+          {items.map((i) => {
+            const product = cartProducts.find((p) => p.id === i.id);
+            if (!product) {
+              return;
+            }
+            return <CartItem key={i.id} count={i.count} {...product} />;
+          })}
+          <div className={styles.line}>
+            <div className={styles.text}>Итог</div>
+            <div className={styles.price}>
+              {total}&nbsp;
+              <span>₽</span>
+            </div>
+          </div>
+          <hr className={styles.hr} />
+          <div className={styles.line}>
+            <div className={styles.text}>Доставка</div>
+            <div className={styles.price}>
+              {DELIVERY_FEE}&nbsp;
+              <span>₽</span>
+            </div>
+          </div>
+          <hr className={styles.hr} />
+          <div className={styles.line}>
+            <div className={styles.text}>
+              Итог <span className={styles.totalCount}>({items.length})</span>
+            </div>
+            <div className={styles.price}>
+              {total + DELIVERY_FEE}&nbsp;
+              <span>₽</span>
+            </div>
+          </div>
+          <div className={styles.checkout}>
+            <Button appearance="big" onClick={checkout}>
+              Оформить
+            </Button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
